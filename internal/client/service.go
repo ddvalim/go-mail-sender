e39e82dc346b8b2595fbb8f7baecce1c00df0520
Diovana Valim <diovanavalimm@gmail.com>
2024-05-18 17:11:40 -0300

    implements client methods

---
+++ internal/client/service.go
@@ -0,0 +1,11 @@
+package client
+
+import (
+	"golang.org/x/oauth2"
+	"net/http"
+)
+
+type Service interface {
+	NewClient(config oauth2.Config, token *oauth2.Token) (*http.Client, error)
+	GetTokenFromFile(file string) (*oauth2.Token, error)
+}
--- internal/client/service_impl.go
+++ internal/client/service_impl.go
@@ -1,1 +1,38 @@
 package client
+
+import (
+	"context"
+	"encoding/json"
+	"golang.org/x/oauth2"
+	"net/http"
+	"os"
+)
+
+type ServiceImpl struct {
+}
+
+func NewService() ServiceImpl {
+	return ServiceImpl{}
+}
+
+func (s ServiceImpl) NewClient(config oauth2.Config, token *oauth2.Token) (*http.Client, error) {
+	return config.Client(context.Background(), token), nil
+}
+
+func (s ServiceImpl) GetTokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	var token oauth2.Token
+
+	err = json.NewDecoder(f).Decode(&token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &token, nil
+}
